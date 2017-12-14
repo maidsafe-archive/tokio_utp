8ad38fb2b86dbb7b22872a97dd12605d7f3de915
Andrew Cann <shum@canndrew.org>
2017-12-14 17:16:07 +0800

    Fix overflow bug in delay calculations

--- src/socket.rs
+++ src/socket.rs
@@ -1184,7 +1184,7 @@ impl Connection {
                     } else if max_sample < 0 {
                         let adjust = -max_sample;
 
-                        self.average_delay_base -= adjust as u32;
+                        self.average_delay_base = self.average_delay_base.saturating_sub(adjust as u32);
                         self.average_delay += adjust;
                         prev_average_delay += adjust;
                     }
