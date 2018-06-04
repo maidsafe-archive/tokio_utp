26fb4ec82f12ea2f220cfff9f66b758181e2f955
Povilas Balciunas <balciunas90@gmail.com>
2018-06-04 16:21:33 +0300

    test/stream: add test that checks if stream read is terminated after shutdown

--- src/test/utp_stream.rs
+++ src/test/utp_stream.rs
@@ -3,7 +3,7 @@ use futures::future::{self, Future};
 use futures::Stream;
 use std::time::Duration;
 use tokio_core::reactor::Core;
-use tokio_io;
+use tokio_io::{self, AsyncRead};
 use UtpSocket;
 
 #[test]
@@ -39,4 +39,36 @@ fn it_receives_data_after_write_shutdown() {
 
     let received = unwrap!(res);
     assert_eq!(received, [1, 2, 3, 4]);
+}
+
+#[test]
+fn when_fin_is_received_read_returns_0_bytes_read() {
+    let mut evloop = unwrap!(Core::new());
+    let handle = evloop.handle();
+
+    let (sock, _) = unwrap!(UtpSocket::bind(&addr!("127.0.0.1:0"), &handle));
+    let (_, listener) = unwrap!(UtpSocket::bind(&addr!("127.0.0.1:0"), &handle));
+    let listener_addr = unwrap!(listener.local_addr());
+
+    let accept_connections = listener
+        .incoming()
+        .into_future()
+        .map_err(|(e, _)| e)
+        .and_then(move |(stream, _incoming)| {
+            let stream = unwrap!(stream);
+            tokio_io::io::shutdown(stream)
+        })
+        .then(|_| Ok(()));
+    handle.spawn(accept_connections);
+
+    let mut data = vec![0; 64];
+    let res = evloop.run(future::lazy(move || {
+        sock.connect(&listener_addr).and_then(|mut stream| {
+            // read until smth arrives or read is terminated by Fin packet
+            future::poll_fn(move || stream.poll_read(&mut data))
+        })
+    }));
+
+    let received = unwrap!(res);
+    assert_eq!(received, 0);
 }
