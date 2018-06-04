cbbc6f1561bf03bed98ab4b8689a9bb64bc70caf
Povilas Balciunas <balciunas90@gmail.com>
2018-06-04 10:25:30 +0300

    refactor/tests: reduce boilerplate code

--- src/socket.rs
+++ src/socket.rs
@@ -1825,17 +1825,22 @@ mod tests {
             (packets_rx, listener_addr)
         }
 
+        /// Reduce some boilerplate.
+        /// Returns socket inner with some common defaults.
+        fn make_socket_inner(evloop: &Core) -> InnerCell {
+            let handle = evloop.handle();
+            let (_listener_registration, listener_set_readiness) = Registration::new2();
+            let socket = unwrap!(UdpSocket::bind(&addr!("127.0.0.1:0"), &handle));
+            Inner::new_shared(&handle, socket, listener_set_readiness)
+        }
+
         mod process_unknown {
             use super::*;
 
             #[test]
             fn when_packet_is_syn_it_schedules_reset_back() {
                 let evloop = unwrap!(Core::new());
-                let handle = evloop.handle();
-                let (_listener_registration, listener_set_readiness) = Registration::new2();
-
-                let socket = unwrap!(UdpSocket::bind(&addr!("127.0.0.1:0"), &handle));
-                let inner = Inner::new_shared(&handle, socket, listener_set_readiness);
+                let inner = make_socket_inner(&evloop);
 
                 let mut packet = Packet::syn();
                 packet.set_connection_id(12_345);
@@ -1853,11 +1858,7 @@ mod tests {
             #[test]
             fn when_packet_is_reset_nothing_is_sent_back() {
                 let evloop = unwrap!(Core::new());
-                let handle = evloop.handle();
-                let (_listener_registration, listener_set_readiness) = Registration::new2();
-
-                let socket = unwrap!(UdpSocket::bind(&addr!("127.0.0.1:0"), &handle));
-                let inner = Inner::new_shared(&handle, socket, listener_set_readiness);
+                let inner = make_socket_inner(&evloop);
 
                 let mut packet = Packet::reset();
                 packet.set_connection_id(12_345);
@@ -1877,12 +1878,9 @@ mod tests {
             fn it_attempts_to_send_all_queued_reset_packets() {
                 let mut evloop = unwrap!(Core::new());
                 let handle = evloop.handle();
+                let inner = make_socket_inner(&evloop);
 
                 let task = future::lazy(|| {
-                    let (_listener_registration, listener_set_readiness) = Registration::new2();
-                    let socket = unwrap!(UdpSocket::bind(&addr!("127.0.0.1:0"), &handle));
-                    let inner = Inner::new_shared(&handle, socket, listener_set_readiness);
-
                     let (packets_rx, remote_peer_addr) = wait_for_packets(&handle);
 
                     let mut packet = Packet::reset();
@@ -1917,11 +1915,7 @@ mod tests {
             #[test]
             fn when_listener_is_closed_it_schedules_reset_packet_back() {
                 let evloop = unwrap!(Core::new());
-                let handle = evloop.handle();
-                let (_listener_registration, listener_set_readiness) = Registration::new2();
-
-                let socket = unwrap!(UdpSocket::bind(&addr!("127.0.0.1:0"), &handle));
-                let inner = Inner::new_shared(&handle, socket, listener_set_readiness);
+                let inner = make_socket_inner(&evloop);
                 unwrap!(inner.write()).listener_open = false;
 
                 let mut packet = Packet::syn();
