b04ec1592defe0a7c962b47b7d36aef8e9872f1a
Povilas Balciunas <balciunas90@gmail.com>
2018-06-05 14:58:22 +0300

    feat/socket: implement graceful connection shutdown
    When connection has received Fin, it has sent it's own Fin and it was acked by remote peer, connection is considered to be gracefully shutdown.
    In such case it's state is `Closed`.
    There's a new method `UtpStream::finalize()`, which creates a future that waits for notification when connection state gets `Closed`.

--- src/test/utp_stream.rs
+++ src/test/utp_stream.rs
@@ -72,3 +72,66 @@ fn when_fin_is_received_read_returns_0_bytes_read() {
     let received = unwrap!(res);
     assert_eq!(received, 0);
 }
+
+mod finalize {
+    use super::*;
+
+    #[test]
+    fn it_waits_for_both_peers_to_shutdown() {
+        let mut evloop = unwrap!(Core::new());
+        let handle = evloop.handle();
+
+        let (sock, _) = unwrap!(UtpSocket::bind(&addr!("127.0.0.1:0"), &handle));
+        let (_, listener) = unwrap!(UtpSocket::bind(&addr!("127.0.0.1:0"), &handle));
+        let listener_addr = unwrap!(listener.local_addr());
+
+        let accept_connections = listener
+            .incoming()
+            .into_future()
+            .map_err(|(e, _)| e)
+            .and_then(move |(stream, _incoming)| {
+                let stream = unwrap!(stream);
+                tokio_io::io::shutdown(stream).and_then(|stream| stream.finalize().infallible())
+            })
+            .then(|_| Ok(()));
+        handle.spawn(accept_connections);
+
+        let res = evloop.run(future::lazy(move || {
+            sock.connect(&listener_addr).and_then(|stream| {
+                tokio_io::io::shutdown(stream).and_then(|stream| stream.finalize().infallible())
+            })
+        }));
+
+        unwrap!(res);
+    }
+
+    #[test]
+    fn it_times_out_when_not_both_peers_shutdown() {
+        let mut evloop = unwrap!(Core::new());
+        let handle = evloop.handle();
+
+        let (sock, _) = unwrap!(UtpSocket::bind(&addr!("127.0.0.1:0"), &handle));
+        let (_, listener) = unwrap!(UtpSocket::bind(&addr!("127.0.0.1:0"), &handle));
+        let listener_addr = unwrap!(listener.local_addr());
+
+        let accept_connections = listener
+            .incoming()
+            .into_future()
+            .map_err(|(e, _)| e)
+            .and_then(move |(stream, _incoming)| {
+                let stream = unwrap!(stream);
+                tokio_io::io::shutdown(stream).and_then(|stream| stream.finalize().infallible())
+            })
+            .then(|_| Ok(()));
+        handle.spawn(accept_connections);
+
+        let res = evloop.run(future::lazy(move || {
+            sock.connect(&listener_addr)
+                .and_then(|stream| stream.finalize().infallible())
+                .with_timeout(Duration::from_secs(2), &handle)
+        }));
+
+        let finalize_timedout = unwrap!(res).is_none();
+        assert!(finalize_timedout);
+    }
+}
